c3d4112ecbc8774b2203d2f02b6684a21b889ed5
Dumpinground <e60533658@icloud.com>
2022-11-19 17:00:04 +0800

    u

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,115 @@
+mod guess_game;
+
 use std::io;
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
-use rand::Rng;
-use std::cmp::Ordering;
 
 fn main() {
-    // say_simply("Hello fellow Rustaceans!");
-    guess();
+
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64
+}
+
+fn first_word(s : &str) -> &str {
+
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn the_slice_type() {
+
+    let mut s = String::from("hello world");
+
+    println!("{}", first_word(&s));
+}
+
+fn ownership() {
+    let s1 = String::from("hello");
+    let s2 = &s1;
+
+    println!("{}, world!", s1);
+}
+
+fn nf(a: i16, b: i16, n: i16) {
+    if n != 0 {
+        print!("{} ", a);
+        nf(b, a + b, n - 1);
+    }
+}
+
+fn break_loop() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+
+    println!("End count = {}", count);
+}
+
+fn plus_one(x: i8) -> i8 {
+    x + 1
+}
+
+fn five() -> i8 {
+    5
+}
+
+fn array() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim().parse()
+        .expect("Index entered was not a nubmer");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is {}",
+        index, element
+    );
 }
 
 fn say_simply(message : &str) {
@@ -16,42 +119,4 @@ fn say_simply(message : &str) {
     let mut writer = BufWriter::new(stdout.lock());
 
     say(message.as_bytes(), width, &mut writer).unwrap();
-}
-
-fn guess() {
-
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
-    loop {
-        println!("The secret number is: {}", secret_number);
-
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("invalid number");
-                continue;
-            },
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            },
-        }
-    }
 }
