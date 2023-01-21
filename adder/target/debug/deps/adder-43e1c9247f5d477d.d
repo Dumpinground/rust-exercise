ca777fb807112318e6f8337d4538020331e01551
Dumpinground <e60533658@icloud.com>
2023-01-21 12:56:46 +0800

    clean

--- adder/target/debug/deps/adder-43e1c9247f5d477d.d
+++
@@ -1,5 +0,0 @@
-/workspaces/rust-exercise/adder/target/debug/deps/adder-43e1c9247f5d477d: src/lib.rs
-
-/workspaces/rust-exercise/adder/target/debug/deps/adder-43e1c9247f5d477d.d: src/lib.rs
-
-src/lib.rs:
--- adder/target/debug/deps/adder-8b62dfea9e560560.d
+++
@@ -1,7 +0,0 @@
-/workspaces/rust-exercise/adder/target/debug/deps/adder-8b62dfea9e560560.rmeta: src/lib.rs
-
-/workspaces/rust-exercise/adder/target/debug/deps/libadder-8b62dfea9e560560.rlib: src/lib.rs
-
-/workspaces/rust-exercise/adder/target/debug/deps/adder-8b62dfea9e560560.d: src/lib.rs
-
-src/lib.rs:
