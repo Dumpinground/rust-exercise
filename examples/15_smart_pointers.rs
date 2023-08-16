542123dd0ab8da1b43319afa9cd2fa3f89eed2f3
Ad <DeadLimit@lkr5.onmicrosoft.com>
2023-08-16 10:11:10 +0800

    add stack ref

--- examples/15_smart_pointers.rs
+++ examples/15_smart_pointers.rs
@@ -1,7 +1,8 @@
-use std::ops::Deref;
+use std::{ops::Deref, rc::Rc, cell::{Cell, RefCell}};
 
 fn main() {
     println!("Hello Smart Pointers");
+    println!("reference:\nhttps://course.rs/advance/smart-pointer/drop.html");
 }
 
 #[test]
@@ -36,4 +37,69 @@ impl<T> MyBox<T> {
 fn my_smart_deref() {
     let y = MyBox::new(5);
     let z = *y;
+}
+
+#[test]
+fn rc() {
+    let s = String::from("hello, world");
+    let a = Rc::new(s);
+    println!("{}", Rc::strong_count(&a));
+    let b = Rc::clone(&a);
+    println!("{}", Rc::strong_count(&b));
+}
+
+#[test]
+fn cell() {
+    let c = Cell::new("asdf");
+    let one = c.get();
+    c.set("qwer");
+    let tow = c.get();
+    println!("{}, {}", one, tow);
+}
+
+#[test]
+fn ref_cell() {
+    let s = Rc::new(RefCell::new("In 1".to_string()));
+    println!("{}", Rc::strong_count(&s));
+    let s1 = s.clone();
+    println!("{}", Rc::strong_count(&s));
+    let s2 = s.clone();
+    println!("{}", Rc::strong_count(&s));
+
+    s2.borrow_mut().push_str(" out 2");
+
+    println!("{:?}\n{:?}\n{:?}", s, s1, s2);
+}
+
+#[derive(Debug)]
+struct Org {
+    name: String,
+    depth: usize,
+    child: Vec<Org>,
+}
+
+impl Org {
+    fn new(name: String, depth: usize) -> Org {
+        Org { name, depth, child: Vec::new() }
+    }
+}
+
+#[test]
+fn vec_ref() {
+    let s = "1".to_string();
+    let texts = vec![("1".to_owned(), 1), ("2".to_owned(), 2), ("3".to_owned(), 2)];
+    let mut stack = Vec::new();
+
+    for (text, indent) in texts {
+        let last = stack.last_mut();
+        match last {
+            None => {
+                stack.push(Org::new(text, indent));
+            }
+            Some(last) => {
+                last.child.push(Org::new(text, indent))
+            }
+        }
+        println!("len: {}, {:?}", stack.len(), stack.last());
+    }
 }
